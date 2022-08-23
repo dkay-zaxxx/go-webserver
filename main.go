e02a064e29f2dd545417fa0a490affcefa6a47e8
dkay-zaxxx <59197203+dkay-zaxxx@users.noreply.github.com>
2022-08-23 15:23:50 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func RequestLogger(targetMux http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		targetMux.ServeHTTP(w, r)
+		requesterIP := r.RemoteAddr
+
+		log.Printf(
+			"%s\t\t%s\t\t%s\t\t%v",
+			r.Method,
+			r.RequestURI,
+			requesterIP,
+			time.Since(start),
+		)
+	})
+}
+
+func main() {
+	fileName := "./logs/log.txt"
+
+	logFile, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("./public_html")))
+
+	log.Fatal(http.ListenAndServe(":8888", RequestLogger(mux)))
+
+}
